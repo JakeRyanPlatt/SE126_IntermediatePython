c594f2031552b9444d7a050e55adbd5cc40bd6e5
Jake <jakeplatt176290@gmail.com>
2025-07-15 11:29:21 -0400

    Update main.py
    Added ASCII text, Main function and control loops finished.

--- main.py
+++ main.py
@@ -1,8 +1,76 @@
+# Import Modules
 import os
-import rand
-games < 10000
+import random
+#=============Functions============#
+def roll_dice():  #function returns some of 2d6
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    sum = d1 + d2
+    return sum
 
-rollDice
+def win_loss_ratio():
+        winloss =  (win/count)
+        return winloss
+#=============Global Variables============#
+count = 0# Couynts number of rounds
+win = 0 # Counts number of wins
+point = 0 # Holds the point value if a point is established
 
-if rollDice == 7 or == 11:
-        wins
+#==================MAIN===================#
+print("")
+print("")
+print("")
+print("     ▄████████    ▄████████    ▄████████  ▄█  ███▄▄▄▄    ▄██████▄ ")
+print("    ███    ███   ███    ███   ███    ███ ███  ███▀▀▀██▄ ███    ███")
+print("    ███    █▀    ███    ███   ███    █▀  ███▌ ███   ███ ███    ███")
+print("    ███          ███    ███   ███        ███▌ ███   ███ ███    ███")
+print("    ███        ▀███████████ ▀███████████ ███▌ ███   ███ ███    ███")
+print("    ███        ▀███████████ ▀███████████ ███▌ ███   ███ ███    ███")
+print("    ███    ███   ███    ███    ▄█    ███ ███  ███   ███ ███    ███")
+print("    ████████▀    ███    █▀   ▄████████▀  █▀    ▀█   █▀   ▀██████▀ ")
+print("")
+print("")
+print("   ▄████████    ▄████████    ▄████████    ▄███████▄    ▄████████")
+print("   ███    ███   ███    ███   ███    ███   ███    ███   ███    ███")
+print("   ███    █▀    ███    ███   ███    ███   ███    ███   ███    █▀ ")
+print("   ███         ▄███▄▄▄▄██▀   ███    ███   ███    ███   ███       ")
+print("   ███        ▀▀███▀▀▀▀▀   ▀███████████ ▀█████████▀  ▀███████████")
+print("   ███    █▄  ▀███████████   ███    ███   ███                 ███")
+print("   ███    ███   ███    ███   ███    ███   ███           ▄█    ███")
+print("   ████████▀    ███    ███   ███    █▀   ▄████▀       ▄████████▀ ")
+print("                ███    ███                                       ")
+print("")
+print("")
+print("")
+#=================Game Logic=================#
+
+while count < 10000:
+    roll_one = roll_dice()
+
+    if roll_one == 7 or roll_one == 11:
+        count += 1
+        win += 1
+    elif roll_one == 2 or roll_one == 3 or roll_one == 12:
+        count += 1
+        win -= 1
+    else:  # Point condition
+        point = roll_one
+        flag = 1
+        while flag == 1:
+            roll_two = roll_dice()
+            if roll_two == point:
+                count += 1 
+                win += 1
+                flag = 0
+            elif roll_two == 7:
+                count += 1
+                win -= 1     
+                flag = 0
+
+print("Total Rounds:", count)
+print(f"Win/Loss Ratio: {win_loss_ratio()*100:.2f}%")
+
+
+
+
+
