6083033cd4b3d8d2fe128fdeb72d137b4561794e
Jake <jakeplatt176290@gmail.com>
2025-07-15 16:01:26 -0400

    Update main.py
    Fixed logical issues, Condensed game logic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Import Modules
-import os
 import random
 #=============Functions============#
 def roll_dice():  #function returns some of 2d6
@@ -9,13 +8,12 @@ def roll_dice():  #function returns some of 2d6
     return sum
 
 def win_loss_ratio():
-        winloss =  (win/count)
+        winloss =  (win+loss) / games
         return winloss
 #=============Global Variables============#
-count = 0# Couynts number of rounds
+loss = 0 # Counts number of rounds
 win = 0 # Counts number of wins
-point = 0 # Holds the point value if a point is established
-
+games = 0
 #==================MAIN===================#
 print("")
 print("")
@@ -44,31 +42,29 @@ print("")
 print("")
 #=================Game Logic=================#
 
-while count < 10000:
-    roll_one = roll_dice()
+while games < 10001:
+    
+    roll_one = roll_dice() # 1 location to roll the dice
 
     if roll_one == 7 or roll_one == 11:
-        count += 1
         win += 1
     elif roll_one == 2 or roll_one == 3 or roll_one == 12:
-        count += 1
-        win -= 1
+        loss += 1  # This might be ruining the win loss ratio
     else:  # Point condition
-        point = roll_one
-        flag = 1
-        while flag == 1:
-            roll_two = roll_dice()
-            if roll_two == point:
-                count += 1 
+        point = roll
+        roll = roll_dice()
+        while roll != point and roll != 7:
+                roll = roll_dice() # second location to roll the dice
+        if roll == point:
                 win += 1
-                flag = 0
-            elif roll_two == 7:
-                count += 1
+        elif roll == 7:
                 win -= 1     
-                flag = 0
-
-print("Total Rounds:", count)
-print(f"Win/Loss Ratio: {win_loss_ratio()*100:.2f}%")
+    games = games + 1
+ 
+#=================End of Game Logic=================#
+print("win = ", win, "Games = ", games)
+print("Total Rounds:", point)
+print(f"Win/Loss Ratio: {win_loss_ratio():.2%}")
 
 
 
